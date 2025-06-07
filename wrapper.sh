a6b980a4f4866d8d3dd707cdb11033fedb994507
gitSerraNando <hernandoescobar23@gmail.com>
2025-06-07 16:41:16 -0500

    Refactor Dockerfile.enrich and wrapper.sh: enhance comments for clarity and ensure executable permissions for the wrapper script

--- wrapper.sh
+++ wrapper.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 PORT=${PORT:-8080}
 
-# 1) HTTP dummy para probes
+# 1) Dummy HTTP para health checks en el puerto esperado
 while true; do
   echo -e "HTTP/1.1 200 OK\n\nok" | nc -l -p "$PORT"
 done &
 
-# 2) Ejecuta el loader/enrich en foreground
-exec /usr/bin/bigquery-loader-pubsub \
+# 2) Ejecuta el enricher real con Java
+exec java \
+  -Dnashorn.args=--language=es6 \
+  -jar /home/snowplow/lib/com.snowplowanalytics.snowplow-enrich-pubsub-5.3.0-launcher.jar \
   --enrichments=/enrichments \
-  --iglu-config=/config/iglu.hocon \
-  --config=/config/loader.hocon
+  --iglu-config=/config/resolver.json \
+  --config=/config/config.hocon
